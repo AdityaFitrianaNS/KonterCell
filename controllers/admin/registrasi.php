853d942ffb59da941a0576e631b53e0e22720dab
Aditya Fitriana NS <aditya.fns04@gmail.com>
2022-08-03 17:32:43 +0700

    feat: perbarui repo

---
+++ controllers/admin/registrasi.php
@@ -0,0 +1,82 @@
+<?php
+
+require "../koneksi.php";
+
+$nama = htmlspecialchars(stripslashes($_POST['nama']));
+$email = htmlspecialchars(stripslashes($_POST['email']));
+$username = htmlspecialchars(stripslashes($_POST['username']));
+$password = mysqli_real_escape_string($conn, $_POST['password']);
+$password2 = mysqli_real_escape_string($conn, $_POST['password2']);
+
+// Konfirm password
+if ($password !== $password2) {
+    echo "<script>
+        alert('Password dengan Konfirmasi Password tidak sesuai!');
+    </script>";
+
+    // Tidak melanjutkan ekseskusi
+    return false;
+}
+// Username tidak boleh duplikat
+$ResultUsername = mysqli_query($conn, "SELECT username from admin_web WHERE username = '$username'");
+
+if (mysqli_fetch_assoc($ResultUsername)) {
+    echo "<script>
+    alert('Akun yang dibuat sudah terdaftar');
+</script>";
+
+    // Tidak melanjutkan eksekusi
+    return false;
+}
+
+// Encrypt password
+$password = password_hash($password, PASSWORD_DEFAULT);
+
+$registrasi = "INSERT INTO admin_web VALUES('','$nama','$email','$username','$password')";
+mysqli_query($conn, $registrasi);
+
+?>
+
+<!-- Script CDN Sweetalert 2 -->
+<script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+<body style="font-family: poppins;"></body>
+
+<?php
+// Cek apakah data berhasil diubah, 
+// jika $registrasi lebih dari 0 artinya data berhasil diubah
+if ($registrasi > 0) {
+?>
+   <!-- alert berhasil -->
+   <script>
+      Swal.fire({
+         icon: 'success',
+         title: 'Berhasil!',
+         text: 'Registrasi berhasil, Silahkan login!'
+      }).then((result) => {
+         if (result.isConfirmed) {
+            document.location.href = '../../views/admin/login';
+         }
+      })
+   </script>
+<?php
+} else {
+?>
+   <!-- alert gagal -->
+   <script>
+      Swal.fire({
+         icon: 'error',
+         title: 'Gagal',
+         text: 'Registrasi Gagal, Silahkan periksa kembali!'
+      }).then((result) => {
+         if (result.isConfirmed) {
+            document.location.href = '../../views/admin/registrasi';
+         }
+      })
+   </script>
+<?php
+}
+
+// Kembalikan jumlah baris yang terpengaruh INSERT sebelumnya
+return mysqli_affected_rows($conn);
+
+?>
---
+++ views/admin/login.php
@@ -0,0 +1,90 @@
+<?php
+
+session_start();
+
+require '../../controllers/koneksi.php';
+
+if (isset($_POST["login"])) {
+    $username = mysqli_real_escape_string($conn, $_POST['username']);
+    $password = mysqli_real_escape_string($conn, $_POST['password']);
+
+    $result = mysqli_query($conn, "SELECT * FROM admin_web WHERE username = '$username'");
+    $user = mysqli_fetch_assoc($result);
+
+    if (mysqli_num_rows($result) == 1) {
+        $row = mysqli_fetch_assoc($result);
+        if (password_verify($password, $user['password'])) {
+            $_SESSION['nama'] = $user['nama'];
+            $_SESSION["login"] = true;
+            echo "
+            <script>
+                alert('Berhasil login!');
+                document.location.href = 'registrasi.php';
+            </script>
+            ";
+            exit;
+        }
+    }
+    $error = true;
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- CSS Bootstrap-->
+    <link rel="stylesheet" href="../../public/css/bootstrap.css">
+    <!-- CSS Bootstrap Icons -->
+    <link rel="stylesheet" href="../../public/css/bootstrap-icons.css">
+    <!-- Custom Styles Navbar -->
+    <link rel="stylesheet" href="../../public/css/login.css">
+    <!-- Icon -->
+    <link rel="shortcun icon" href="../../public/img/icon.png">
+    <!-- Tittle Page -->
+    <title>Login KonterCell</title>
+</head>
+<body>
+    <section>
+        <div class="imgBx">
+            <img src="../../public/img/icon.png" alt="backgroundForm" class="" >
+        </div>
+        <div class="contentBx">
+            <div class="formBx">
+                <h2 class="mt-4">Login</h2>
+                <?php if (isset($error)) : ?>
+                    <script>
+                        Swal.fire({
+                            icon: 'error',
+                            title: 'Oops...',
+                            text: 'Username atau Password salah!'
+                        });
+                    </script>
+                <?php endif; ?>
+                <form action="" method="POST">
+                    <div class="inputBx">
+                        <label for="username" class="label">Username</label>
+                        <input type="text" name="username" id="username" required>
+                    </div>
+                    <div class="inputBx">
+                        <label for="password" class="label">Password</label>
+                        <input type="password" name="password" id="password">
+                    </div>
+                    <div class="remember">
+                        <label><input type="checkbox" name="">Remember me</label>
+                    </div>
+                    <div class="inputBx">
+                        <input type="submit" class="tombol" name="login" id="login" value="Login">
+                    </div>
+                    <div class="inputBx">
+                        <p>Tidak memiliki akun? <a href="registrasi.php" data-bs-toggle="modal" data-bs-target="#registrasi_modal"> Registrasi</a></p>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </section>
+</body>
+</html>
